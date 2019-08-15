8eb9bf8df2638c7c2018fe869e5c5031e9954b57
Josh Fischer <josh@joshfischer.io>
2019-08-15 14:04:32 -0500

    test-commit

--- website2/website/scripts/python-doc-gen.sh
+++ website2/website/scripts/python-doc-gen.sh
@@ -21,10 +21,11 @@ sudo apt-get update && sudo apt install -y automake cmake libtool-bin g++ \
  python-setuptools python-dev python-wheel python python-pip unzip tree openjdk-8-jdk virtualenv
 
 # Install Bazel 0.26
-wget -O ./bazel-0.26.0-installer-linux-x86_64.sh https://github.com/bazelbuild/bazel/releases/download/0.26.0/bazel-0.26.0-installer-linux-x86_64.sh && \
-chmod +x ./bazel-0.26.0-installer-linux-x86_64.sh && \
-./bazel-0.26.0-installer-linux-x86_64.sh --user && \
-export PATH="$PATH:$HOME/bin"
+
+#wget -O ./bazel-0.26.0-installer-linux-x86_64.sh https://github.com/bazelbuild/bazel/releases/download/0.26.0/bazel-0.26.0-installer-linux-x86_64.sh && \
+#chmod +x ./bazel-0.26.0-installer-linux-x86_64.sh && \
+#./bazel-0.26.0-installer-linux-x86_64.sh --user && \
+#export PATH="$PATH:$HOME/bin"
 
 set -e
 
@@ -35,7 +36,7 @@ cd ${HERON_ROOT_DIR}
 # ./bazel_configure.py
 
 # Generate python whl packages, packages will be generated in ${HERON_ROOT_DIR}/bazel-genfiles/scripts/packages/
-bazel build --config=ubuntu scripts/packages:pypkgs
+bazel build --config=darwin scripts/packages:pypkgs
 
 cd website2/website/
 mkdir -p ./tmp/
